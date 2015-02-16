536f53e2e11a3d4ed0f0c2c8ba2fbc986a2c8c88
Chris Beer <cabeer@stanford.edu>
2015-02-16 09:21:33 -0800

    Use routing constraints to provide the correct id for rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,7 @@
-module Channels::Routes
-  extend ActiveSupport::Concern
-  def solr_document(primary_resource)
-    add_routes do |options|
-      args = { only: [:show] }
-      args[:constraints] = options[:constraints] if options[:constraints]
-      
-      get "record/:provider_id/:record_id", args.merge(to: "#{primary_resource}#show", as: "solr_document")
-      post "record/:provider_id/:record_id/track", args.merge(to: "#{primary_resource}#track", as: "track_solr_document")
-    end
-  end
-  
-  def bookmarks(_)
-    add_routes do |options|
-      delete "bookmarks/clear", to: "bookmarks#clear", as: "clear_bookmarks"
-      
-      resources :bookmarks, only: [ :index, :create, :new ]
-      get "bookmarks/:provider_id/:record_id/edit(.:format)", to: "bookmarks#edit", as: "edit_bookmark"
-      get "bookmarks/:provider_id/:record_id(.:format)", to: "bookmarks#show", as: "bookmark"
-      patch "bookmarks/:provider_id/:record_id(.:format)", to: "bookmarks#update"
-      put "bookmarks/:provider_id/:record_id(.:format)", to: "bookmarks#update"
-      delete "bookmarks/:provider_id/:record_id(.:format)", to: "bookmarks#destroy"
-    end
-  end
-end
-Blacklight::Routes.send(:include, Channels::Routes)
-
 Rails.application.routes.draw do
   root :to => "channels#index"
 
-#  devise_for :users
-  
-  blacklight_for :catalog
+  blacklight_for :catalog, constraints: { id: /[^\/]+(\/|%2F)[^\/]+/ }
   resources :channels, only: [ :show, :index ]
+  get '/records/:id', to: 'catalog#show', as: 'europeana_document', constraints: { id: /[^\/]+(\/|%2F)[^\/]+/ }
 end
